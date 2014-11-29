a313c66513353a9f0bd7cfeb8a1475c3d50e7bb6
Brian Hsu <brianhsu.hsu@gmail.com>
2014-11-29 20:13:30 +0800

    Cleanup code.

--- src/main/scala/code/comet/PingComet.scala
+++ src/main/scala/code/comet/PingComet.scala
@@ -9,7 +9,6 @@ import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JE.JsRaw
 import code.lib._
 
-case object PushResult
 case class Display(deadMachines: List[(IPAddress, Boolean)])
 case class DeadMachine(ipAddress: IPAddress)
 case class LiveMachine(ipAddress: IPAddress)
@@ -21,40 +20,43 @@ case class IPAddress(net: String, host: Int) {
 object PingServer extends LiftActor with ListenerManager {
 
   private var machineStatus: Map[IPAddress, Boolean] = Map.empty
-
   def createUpdate = Display(machineStatus.toList.sortWith(_._1.sortString < _._1.sortString))
 
-  def ping(ip: IPAddress): Unit = new Thread {
+  private def updateWebPage() {
+     updateListeners()
+     Schedule(() => updateWebPage(), 5000)
+  }
 
-    override def run() {
-      val inet = InetAddress.getByName(ip.toString)
-      var counter = 0
-      var isConnected = inet.isReachable(500)
+  def ping(ip: IPAddress) {
 
-      while (counter < 9 && !isConnected) {
-        isConnected = inet.isReachable(500)
-        counter += 1
+    val thread = new Thread {
+      override def run() {
+        val inet = InetAddress.getByName(ip.toString)
+        var counter = 0
+        var isConnected = inet.isReachable(500)
+
+        while (counter < 9 && !isConnected) {
+          isConnected = inet.isReachable(500)
+          counter += 1
+        }
+
+        val message = if (isConnected) LiveMachine(ip) else DeadMachine(ip)
+        PingServer ! message
+        Schedule(() => ping(ip), 5000)
       }
-
-      val message = if (isConnected) LiveMachine(ip) else DeadMachine(ip)
-      PingServer ! message
-      Schedule(() => ping(ip), 5000)
     }
 
-  }.start()
+    thread.start()
+  }
 
-  override def lowPriority : PartialFunction[Any,Unit] = {
+  override def lowPriority = {
     case DeadMachine(ip) => machineStatus = machineStatus.updated(ip, false)
     case LiveMachine(ip) => machineStatus = machineStatus.updated(ip, true)
-    case PushResult => 
-      updateListeners()
-      Schedule(() => this ! PushResult, 5000)
   }
 
   (1 to 254).foreach(i => ping(IPAddress("192.168.10", i)))
   (1 to 76).foreach(i => ping(IPAddress("192.168.20", i)))
- 
-  this ! PushResult
+  updateWebPage()
 }
 
 class PingComet extends CometActor with CometListener {
@@ -62,7 +64,6 @@ class PingComet extends CometActor with CometListener {
   case class FillColor(ip: IPAddress, color: String)
 
   private var deadMachines: List[(IPAddress, Boolean)] = Nil
-
   def registerWith = PingServer
 
   def render = {
@@ -78,7 +79,7 @@ class PingComet extends CometActor with CometListener {
     partialUpdate(JsRaw("""$('.exception').css("fill", "#cccccc");"""))
   }
 
-  def updateTable(newDeadMachines: List[(IPAddress, Boolean)]) {
+  def updateTable(newDeadMachines: List[(IPAddress, Boolean)]) = {
     deadMachines = newDeadMachines
     deadMachines.foreach { case(ip, isConnected) =>
       val color = if (isConnected) "#00FF00" else "#FF0000"
@@ -87,7 +88,7 @@ class PingComet extends CometActor with CometListener {
     reRender(false)
   }
 
-  override def lowPriority : PartialFunction[Any,Unit] = {
+  override def lowPriority = {
     case Display(newDeadMachines) => updateTable(newDeadMachines)
     case FillColor(ip, color) => fillColor(ip, color)
   }
