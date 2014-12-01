4691b6538a89da86d08e46563e0b6bef6cb7029a
BrianHsu <brianhsu.hsu@gmail.com>
2014-12-01 10:54:54 +0800

    Add machine detail info in alive page.

--- src/main/scala/code/comet/PingComet.scala
+++ src/main/scala/code/comet/PingComet.scala
@@ -9,29 +9,31 @@ import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JE.JsRaw
 import code.lib._
 
-case class Display(deadMachines: List[(IPAddress, Boolean)])
-case class DeadMachine(ipAddress: IPAddress)
-case class LiveMachine(ipAddress: IPAddress)
-case class IPAddress(net: String, host: Int) {
-  override def toString = s"$net.$host"
-  def sortString = f"$net.$host%03d"
-}
+case class UpdatePage(liveMachines: List[MachineInfo], deadMachines: List[MachineInfo])
+case class DeadMachine(machineInfo: MachineInfo)
+case class LiveMachine(machineInfo: MachineInfo)
 
 object PingServer extends LiftActor with ListenerManager {
 
-  private var machineStatus: Map[IPAddress, Boolean] = Map.empty
-  def createUpdate = Display(machineStatus.toList.sortWith(_._1.sortString < _._1.sortString))
+  private var deadMachines: Set[MachineInfo] = Set.empty
+  private var liveMachines: Set[MachineInfo] = Set.empty
+  private def ipSorting(ip: String) = ip.split("\\.").map(x => String.format(f"${x.toInt}%03d")).mkString
+
+  def createUpdate = UpdatePage(
+    liveMachines.toList.sortWith((x, y) => ipSorting(x.ip) < ipSorting(y.ip)), 
+    deadMachines.toList.sortWith((x, y) => ipSorting(x.ip) < ipSorting(y.ip))
+  )
 
   private def updateWebPage() {
      updateListeners()
      Schedule(() => updateWebPage(), 5000)
   }
 
-  def ping(ip: IPAddress) {
+  def ping(ip: String) {
 
     val thread = new Thread {
       override def run() {
-        val inet = InetAddress.getByName(ip.toString)
+        val inet = InetAddress.getByName(ip)
         var counter = 0
         var isConnected = inet.isReachable(500)
 
@@ -40,8 +42,11 @@ object PingServer extends LiftActor with ListenerManager {
           counter += 1
         }
 
-        val message = if (isConnected) LiveMachine(ip) else DeadMachine(ip)
-        PingServer ! message
+        MachineInfo.ipTable.get(ip).foreach { machineInfo =>
+          val message = if (isConnected) LiveMachine(machineInfo) else DeadMachine(machineInfo)
+          PingServer ! message
+        }
+
         Schedule(() => ping(ip), 5000)
       }
     }
@@ -50,46 +55,53 @@ object PingServer extends LiftActor with ListenerManager {
   }
 
   override def lowPriority = {
-    case DeadMachine(ip) => machineStatus = machineStatus.updated(ip, false)
-    case LiveMachine(ip) => machineStatus = machineStatus.updated(ip, true)
+    case DeadMachine(machineInfo) => 
+      liveMachines -= machineInfo
+      deadMachines += machineInfo
+    case LiveMachine(machineInfo) =>
+      liveMachines += machineInfo
+      deadMachines -= machineInfo
   }
 
-  (1 to 254).foreach(i => ping(IPAddress("192.168.10", i)))
-  (1 to 76).foreach(i => ping(IPAddress("192.168.20", i)))
+  (1 to 252).foreach(i => ping(s"192.168.10.$i"))
+  (1 to 76).foreach(i => ping(s"192.168.20.$i"))
   updateWebPage()
 }
 
 class PingComet extends CometActor with CometListener {
   
-  case class FillColor(ip: IPAddress, color: String)
+  case class FillColor(machineInfo: MachineInfo, color: String)
 
-  private var deadMachines: List[(IPAddress, Boolean)] = Nil
+  private var liveMachines: List[MachineInfo] = Nil
+  private var deadMachines: List[MachineInfo] = Nil
+
   def registerWith = PingServer
 
   def render = {
-    ".row" #> deadMachines.map { case(ip, status) =>
-      ".ip *" #> ip.toString &
+    ".row" #> deadMachines.map { case machineInfo =>
+      ".machineID *" #> machineInfo.machineID &
+      ".machineType *" #> machineInfo.model &
+      ".machineNote *" #> machineInfo.note &
+      ".ip *" #> machineInfo.ip &
       ".date *" #> (new Date).toString
     }
   }
 
-  def fillColor(ip: IPAddress, color: String) = {
-    val machineID = MachineInfo.machineID(ip.toString)
-    partialUpdate(JsRaw(s"""jQuery("#$machineID").css("fill", "$color")"""))
+  def fillColor(machineInfo: MachineInfo, color: String) = {
+    partialUpdate(JsRaw(s"""jQuery("#${machineInfo.machineID}").css("fill", "$color")"""))
     partialUpdate(JsRaw("""$('.exception').css("fill", "#cccccc");"""))
   }
 
-  def updateTable(newDeadMachines: List[(IPAddress, Boolean)]) = {
-    deadMachines = newDeadMachines
-    deadMachines.foreach { case(ip, isConnected) =>
-      val color = if (isConnected) "#00FF00" else "#FF0000"
-      this ! FillColor(ip, color)
-    }
+  def updateTable(liveMachines: List[MachineInfo], deadMachines: List[MachineInfo]) = {
+    this.liveMachines = liveMachines
+    this.deadMachines = deadMachines
+    deadMachines.foreach { machineInfo => this ! FillColor(machineInfo, "#FF0000") }
+    liveMachines.foreach { machineInfo => this ! FillColor(machineInfo, "#00FF00") }
     reRender(false)
   }
 
   override def lowPriority = {
-    case Display(newDeadMachines) => updateTable(newDeadMachines)
+    case UpdatePage(liveMachines, deadMachines) => updateTable(liveMachines, deadMachines)
     case FillColor(ip, color) => fillColor(ip, color)
   }
 }
