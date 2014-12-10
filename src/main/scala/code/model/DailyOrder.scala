76f44f8758e47b41eb85c5208d170ff68567754c
BrianHsu <brianhsu.hsu@gmail.com>
2014-12-10 10:54:54 +0800

    Move DailyOrder model to separtate file.

---
+++ src/main/scala/code/model/DailyOrder.scala
@@ -0,0 +1,24 @@
+package code.model
+
+import code.lib._
+
+import net.liftweb.mongodb.record.MongoRecord
+import net.liftweb.mongodb.record.MongoMetaRecord
+import net.liftweb.mongodb.record.field._
+import net.liftweb.record.field._
+
+object DailyOrder extends DailyOrder with MongoMetaRecord[DailyOrder] {
+  override def collectionName = "dailyOrder"
+}
+
+class DailyOrder extends MongoRecord[DailyOrder] with ObjectIdPk[DailyOrder] {
+  def meta = DailyOrder
+
+  val timestamp = new StringField(this, 10)
+  val lotNo = new StringField(this, 100)
+  val order = new StringField(this, 100)
+  val customer = new StringField(this, 100)
+  val product = new StringField(this, 100)
+  val status = new IntField(this, -1)
+}
+
