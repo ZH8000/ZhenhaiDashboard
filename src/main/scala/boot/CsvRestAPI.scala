e6c55d8ba2384362c675cb1c9b29bee87247f4e5
Brian Hsu <brianhsu.hsu@gmail.com>
2014-12-24 13:15:39 +0800

    Add csv export in by capacity view.

--- src/main/scala/boot/CsvRestAPI.scala
+++ src/main/scala/boot/CsvRestAPI.scala
@@ -53,6 +53,16 @@ object CsvRestAPI extends RestHelper {
 
   })
 
+  serve("api" / "csv" / "capacity" prefix {
+    case Nil Get req => toCSVResponse(CapacityCSV.overview)
+    case step :: Nil Get req => toCSVResponse(CapacityCSV(step))
+    case step :: capacity :: Nil Get req => toCSVResponse(CapacityCSV(step, capacity))
+    case step :: capacity :: AsInt(year) :: AsInt(month) :: Nil Get req => toCSVResponse(CapacityCSV(step, capacity, year, month))
+    case step :: capacity :: AsInt(year) :: AsInt(month) :: AsInt(week) :: Nil Get req => toCSVResponse(CapacityCSV(step, capacity, year, month, week))
+    case step :: capacity :: AsInt(year) :: AsInt(month) :: AsInt(week) :: AsInt(date) :: Nil Get req => toCSVResponse(CapacityCSV(step, capacity, year, month, week, date))
+    case step :: capacity :: AsInt(year) :: AsInt(month) :: AsInt(week) :: AsInt(date) :: machineID :: Nil Get req => toCSVResponse(CapacityCSV(step, capacity, year, month, week, date, machineID))
+  })
+
   serve("api" / "csv" prefix {
     case "orderStatus" :: Nil Get req => toCSVResponse(OrderStatusCSV())
     case "todayOrder" :: Nil Get req => toCSVResponse(TodayOrderCSV())
---
+++ src/main/scala/code/csv/CapacityCSV.scala
@@ -0,0 +1,54 @@
+package code.csv
+
+import code.lib._
+import code.json._
+
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
+
+object CapacityCSV {
+
+  def overview = {
+    val JArray(jsonData) = CapacityJSON.overview \\ "dataSet"
+    CSVConverter(List("工序", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String) = {
+    val JArray(jsonData) = CapacityJSON(step) \\ "dataSet"
+    CSVConverter(List("容量", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String, capacity: String) = {
+    val JArray(jsonData) = CapacityJSON(step, capacity) \\ "dataSet"
+    CSVConverter(List("年月", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String, capacity: String, year: Int, month: Int) = {
+    val JArray(jsonData) = CapacityJSON(step, capacity, year, month) \\ "dataSet"
+    CSVConverter(List("週別", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String, capacity: String, year: Int, month: Int, week: Int) = {
+    val JArray(jsonData) = CapacityJSON(step, capacity, year, month, week) \\ "dataSet"
+    CSVConverter(List("日期", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String, capacity: String, year: Int, month: Int, week: Int, date: Int) = {
+    val JArray(jsonData) = CapacityJSON(step, capacity, year, month, week, date) \\ "dataSet"
+    CSVConverter(List("機台", "數量"), List("name", "value"), jsonData)
+  }
+
+  def apply(step: String, capacity: String, year: Int, month: Int, week: Int, date: Int, machineID: String) = {
+    val JArray(jsonData) = CapacityJSON(step, capacity, year, month, week, date, machineID) \\ "dataSet"
+
+    CSVConverter(
+      List("日期", "生產數量", "錯誤數量", "錯誤種類"), 
+      List("timestamp", "count_qty", "bad_qty", "defact_id"), 
+      machineID,
+      "說明", "defact_id",
+      jsonData
+    )
+  }
+
+}
+
