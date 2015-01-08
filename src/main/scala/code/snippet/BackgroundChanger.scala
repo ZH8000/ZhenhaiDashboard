267f366a4fa8604c55c36e10651d85b51e5b0b1f
Brian Hsu <brianhsu.hsu@gmail.com>
2015-01-08 14:47:03 +0800

    Add background color changer.

---
+++ src/main/scala/code/snippet/BackgroundChanger.scala
@@ -0,0 +1,36 @@
+package code.snippet
+
+import code.lib._
+
+import net.liftweb.util.Helpers._
+import net.liftweb.http.SHtml
+import net.liftweb.http.SessionVar
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.JE._
+
+import net.liftweb.util._
+
+object BackgroundColor extends SessionVar[Option[String]](None)
+
+class BackgroundChanger {
+
+  def setupColor(color: String): JsCmd = {
+
+    if (color.startsWith("#") && color.size == 7) {
+      BackgroundColor(Option(color))
+    }
+
+    JsRaw(s"""$$('body').css('background-color', '$color')""").cmd &
+    JsRaw(s"""$$('html').css('background-color', '$color')""").cmd
+  }
+
+  def render = {
+    val bgColor = BackgroundColor.get.getOrElse("#CEECF5")
+    "#bgColorJS *" #> OnLoad(JsRaw(s"""$$('body').css('background-color', '$bgColor'); $$('html').css('background-color', '$bgColor');""").cmd ) &
+    "#bgColor [value]" #> bgColor &
+    "#bgColor [onchange]" #> SHtml.onEvent(setupColor _)
+  }
+}
+
+
