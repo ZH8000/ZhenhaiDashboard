c0e73f998dd2f87eb12f05742a99483b69931b85
BrianHsu <brianhsu.hsu@gmail.com>
2015-04-30 13:56:40 +0000

    Implement new maintenace barcode PDF layout.

--- src/main/scala/code/lib/MaintenanceCodePDF.scala
+++ src/main/scala/code/lib/MaintenanceCodePDF.scala
@@ -16,11 +16,10 @@ object MaintenanceCodePDF {
   val chineseFont = new Font(baseFont, 6) 
   val titleFont = new Font(baseFont, 10)
 
-  def createBarcodeLabel(code: Int, step: String, description: String, pdfWriter: PdfWriter) = {
+  def createBarcodeLabel(code: Int, description: String, pdfWriter: PdfWriter) = {
 
     val cell = new PdfPCell
     val barCode = new Barcode39()
-    val stepTag = new Paragraph(s"$step", chineseFont)
     val descriptionTag = new Paragraph(s"$description", chineseFont)
 
     barCode.setCode("UUU" + code)
@@ -43,10 +42,30 @@ object MaintenanceCodePDF {
     pdfWriter.setPageEmpty(false)
   }
 
-  def createPDF(outputStream: OutputStream) = {
-    val document = new Document(PageSize.A4)
-    val pdfWriter = PdfWriter.getInstance(document, outputStream)
+  def createCell(stepTitle: String, stepCode: Int, pdfWriter: PdfWriter) = {
+    val outterCell = new PdfPCell
+    val titleParagraph = new Paragraph(stepTitle, titleFont)
     val table = new PdfPTable(1)
+
+    titleParagraph.setAlignment(Element.ALIGN_CENTER)
+    outterCell.setHorizontalAlignment(Element.ALIGN_CENTER)
+
+    for {
+      codeMapping <- MaintenanceCode.mapping.get(stepCode)
+      code        <- 1 to 9
+      codeTitle   <- codeMapping.get(code)
+    } {
+      table.addCell(createBarcodeLabel(code, codeTitle, pdfWriter))
+    }
+    table.completeRow()
+    outterCell.addElement(titleParagraph)
+    outterCell.addElement(table)
+    outterCell
+  }
+
+  def createPDF(outputStream: OutputStream) = {
+    val document = new Document(PageSize.A4.rotate)
+    val pdfWriter = PdfWriter.getInstance(document, outputStream)
     val steps = 
       (1 -> "加締卷取") ::
       (2 -> "組立") ::
@@ -57,28 +76,14 @@ object MaintenanceCodePDF {
     document.open()
     pdfWriter.setPageEmpty(false)
 
-    for {
-      (step, stepTitle) <- steps
-    } {
-      val pageTitle = new Paragraph(stepTitle, titleFont)
-      pageTitle.setAlignment(Element.ALIGN_CENTER)
-      pageTitle.setSpacingAfter(10)
-      document.add(pageTitle)
-      val table = new PdfPTable(1)
-      table.setWidthPercentage(20f)
-
-      for {
-        code <- 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: 7 :: 8 :: 9 :: Nil
-        codeMapping <- MaintenanceCode.mapping.get(step)
-        codeTitle <- codeMapping.get(code)
-      } {
-        table.addCell(createBarcodeLabel(code, stepTitle, codeTitle, pdfWriter))
-      }
-      table.completeRow()
-      document.add(table)
-      document.newPage()
-    }
-
+    val table = new PdfPTable(3)
+    table.addCell(createCell("加締", 1, pdfWriter))
+    table.addCell(createCell("組立", 2, pdfWriter))
+    table.addCell(createCell("老化", 3, pdfWriter))
+    table.addCell(createCell("選別", 4, pdfWriter))
+    table.addCell(createCell("加工切角", 5, pdfWriter))
+    table.completeRow()
+    document.add(table)
     document.close()
   }
 
