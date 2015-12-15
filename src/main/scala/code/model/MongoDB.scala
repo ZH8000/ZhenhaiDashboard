0685f1c27c4a99c20a295cbe766d2e357849f01e
Brian Hsu <brianhsu.hsu@gmail.com>
2015-12-15 14:21:00 +0800

    Add sitename and hide announcement management page.

--- src/main/scala/code/model/MongoDB.scala
+++ src/main/scala/code/model/MongoDB.scala
@@ -4,7 +4,7 @@ import com.mongodb.casbah.Imports._
 import net.liftweb.util.Props
 
 object MongoDB {
-  val DatabaseName: String = "zhenhaiSZ"
+  val DatabaseName: String = "zhenhai"
 
   lazy val client = MongoClient()
   lazy val zhenhaiDB = client(DatabaseName)
