8cf91e83f65318e5c3918f9e5949bc362ecfa6e8
Brian Hsu <brianhsu.hsu@gmail.com>
2015-12-21 14:19:09 +0800

    Split MachineCounter to spearate file.

--- src/main/scala/code/model/Alarm.scala
+++ src/main/scala/code/model/Alarm.scala
@@ -1,37 +1,13 @@
 package code.model
 
-import com.mongodb.casbah.Imports._
-
-import code.lib._
-
 import net.liftweb.mongodb.record.MongoRecord
 import net.liftweb.mongodb.record.MongoMetaRecord
 import net.liftweb.mongodb.record.field._
 import net.liftweb.record.field._
-import net.liftweb.sitemap.Loc._
-import net.liftweb.http.OutputStreamResponse
-import net.liftweb.common._
 
-import java.util.Calendar
-import java.util.Date
-import java.text.SimpleDateFormat
-
-object MachineCounter extends MachineCounter with MongoMetaRecord[MachineCounter] {
-  override def collectionName = "machineCounter"
-
-  def getCount(machineID: String): Long = MachineCounter.find("machineID", machineID).map(_.counter.get).getOrElse(0)
-  def toHashMap: Map[String, Long] = MachineCounter.findAll.map(x => x.machineID.get -> x.counter.get).toMap
-}
-
-class MachineCounter extends MongoRecord[MachineCounter] with ObjectIdPk[MachineCounter] {
-  def meta = MachineCounter
-
-  val machineID = new StringField(this, 10)
-  val counter = new LongField(this, 0)
-}
-
-
-
+/**
+ *  記錄
+ */
 object Alarm extends Alarm with MongoMetaRecord[Alarm] {
   override def collectionName = "alarm"
 }
@@ -51,7 +27,5 @@ class Alarm extends MongoRecord[Alarm] with ObjectIdPk[Alarm] {
   val lastReplaceCount = new LongField(this, 0)
 
   def isUrgentEvent = lastReplaceCount.get + countdownQty.get <= MachineCounter.getCount(machineID.get)
-
-
 }
 
