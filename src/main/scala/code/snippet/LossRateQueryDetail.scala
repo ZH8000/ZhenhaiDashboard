928ee8bb818f0557c92299e2841c1481b491146f
Brian Hsu <brianhsu.hsu@gmail.com>
2016-05-04 14:57:55 +0800

    Add comments.

--- src/main/scala/code/snippet/LossRateQueryDetail.scala
+++ src/main/scala/code/snippet/LossRateQueryDetail.scala
@@ -11,14 +11,47 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import scala.util.Try
 
+/**
+ *  用來顯示「損耗查詢」結果第二頁機台每日結果的 Snippet
+ */
 class LossRateQueryDetial {
 
+  /**
+   *  用來代表 MongoDB 查詢結果的原始資料
+   *
+   *  @param    shiftDate     工班日期
+   *  @param    countQty      良品數
+   *  @param    badQty        不良品數
+   *  @param    lossMoney     損耗金額
+   */
   case class RawData(shiftDate: String, countQty: Long, badQty: Long, lossMoney: BigDecimal)
+
+  /**
+   *  用來代表
+   *
+   *  @param    shiftDate     工班日期
+   *  @param    countQty      良品數
+   *  @param    badQty        不良品數
+   *  @param    lossRate      損耗率
+   *  @param    lossMoney     損耗金額
+   */
   case class ResultTableRow(shiftDate: String, countQty: Long, badQty: Long, lossRate: Double, lossMoney: BigDecimal)
 
+  /**
+   *  取得排序過後的表格資料
+   *
+   *  @param    startDate     開始日期
+   *  @param    endDate       結束日期
+   *  @param    machineID     機台編號
+   *  @return                 結果列表
+   */
   def getSortedLossRate(startDate: String, endDate: String, machineID: String): List[ResultTableRow] = {
     val table = MongoDB.zhenhaiDB("dailyLossRate")
+
+    // 查詢資料表裡的 shiftDate 在 startDate 到 endDate 之間，並且 machineID 符合設定的資料
     val rawMongoDBRecord = table.find($and("shiftDate" $gte startDate $lte endDate, "machineID" $eq machineID)).toList
+
+    // 將上述的資料轉為 List[RawData] 以方便處理
     val rawDataList = rawMongoDBRecord.map { record =>
       val shiftDate = record.get("shiftDate").toString
       val fullProductCode = Try{record.get("partNo").toString.substring(10, 15)}.getOrElse("Unknown")
@@ -28,7 +61,12 @@ class LossRateQueryDetial {
       val countQty = Try{record.get("count_qty").toString.toLong}.getOrElse(0L)
       RawData(shiftDate, countQty, badQty, lossMoney)
     }
+
+    // 將上述資料以 shiftDate 做 grouping，成為 Map[shiftDate, List[RawData]]
     val groupedData = rawDataList.groupBy(_.shiftDate)
+
+    // 最後將上述 Map 中每一個 shiftDate 的 Value 的 List[RawData] 列表做加總，
+    // 最後轉為一個 List[ResultTableRow]，每一個 element 是一個 shiftDate
     val aggreatedData = groupedData.map { case (shiftDate, data) =>
       val totalCountQty = data.map(_.countQty).sum
       val totalBadQty = data.map(_.badQty).sum
@@ -37,9 +75,16 @@ class LossRateQueryDetial {
       ResultTableRow(shiftDate, totalCountQty, totalBadQty, lossRate, totalLossMoney)
     }
 
+    // 最後以 shiftDate 做降冪排序
     aggreatedData.toList.sortWith(_.shiftDate < _.shiftDate)
   }
 
+  /**
+   *  顯示表格的 Binding
+   *
+   *  @param    tableData     結果表格的資料
+   *  @return                 用來 Binding 的設定
+   */
   def showDataTable(tableData: List[ResultTableRow]) = {
     val startDate = S.param("startDate").getOrElse("")
     val endDate = S.param("endDate").getOrElse("")
@@ -58,6 +103,9 @@ class LossRateQueryDetial {
     }
   }
 
+  /**
+   *  顯示結果表格
+   */
   def render = {
     val startDateHolder = S.param("startDate")
     val endDateHolder = S.param("endDate")
--- src/main/scala/code/snippet/LossRateQueryResult.scala
+++ src/main/scala/code/snippet/LossRateQueryResult.scala
@@ -15,12 +15,38 @@ import org.joda.time.format.DateTimeFormat
 import org.joda.time.LocalDate
 
 
-
+/**
+ *  用來顯示「損耗查詢」結果第一頁機台排名列表的 Snippet
+ */
 class LossRateQueryResult {
 
+  /**
+   *  用來代表 MongoDB 裡的 raw data 的資料
+   *
+   *  @param    machineID       機台編號
+   *  @param    countQty        良品數
+   *  @param    badQty          不良品數
+   *  @param    lossMoney       損耗金額
+   */
   case class RawData(machineID: String, countQty: Long, badQty: Long, lossMoney: BigDecimal)
+
+  /**
+   *  用來代表顯示排名結果的表格的其中一列
+   *
+   *  @param    machineID     機台編號
+   *  @param    countQty      良品數
+   *  @param    badQty        不良品數
+   *  @param    lossMoney     損耗金額
+   */
   case class ResultTableRow(machineID: String, countQty: Long, badQty: Long, lossRate: Double, lossMoney: BigDecimal)
 
+  /**
+   *  取得兩個以 yyyy-MM-dd 表示的日期間的
+   *
+   *  @param    firstDateString       第一個日期
+   *  @param    secondDateString      第二個日期
+   *  @return                         兩個人期中間隔了幾個日曆天
+   */
   def getDaysBetween(firstDateString: String, secondDateString: String): Int = {
     val dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
     val firstDate = dateFormatter.parseDateTime(firstDateString)
@@ -28,9 +54,21 @@ class LossRateQueryResult {
     Days.daysBetween(new LocalDate(secondDate), new LocalDate(firstDate)).getDays.abs
   }
 
+  /**
+   *  取得排序過後的損耗率列表
+   *
+   *  @param    startDate     開始日期
+   *  @param    endDate       結束日期
+   *  @param    machineType   機台類型（1 = 加締 / 2 = 組立 / 3 = 老化 / 4 = 選別 / 5 = 加工切腳）
+   */
   def getSortedLossRate(startDate: String, endDate: String, machineType: String): List[ResultTableRow] = {
     val table = MongoDB.zhenhaiDB("dailyLossRate")
+    
+    // 查詢 shiftDate 介於 startDate 與 endDate 之間，並且
+    // 其 machineType 符合設定的資料
     val rawMongoDBRecord = table.find($and("shiftDate" $gte startDate $lte endDate, "machineType" $eq machineType.toInt)).toList
+
+    // 將 MongoDB 回傳的資料轉為 List[RawData] 以方便處理
     val rawDataList = rawMongoDBRecord.map { record =>
       val fullProductCode = Try{record.get("partNo").toString.substring(10, 15)}.getOrElse("Unknown")
       val badQty = Try{record.get("event_qty").toString.toLong}.getOrElse(0L)
@@ -41,7 +79,11 @@ class LossRateQueryResult {
 
       RawData(machineID, countQty, badQty, lossMoney)
     }
+
+    // 將上述的資料，以 machineID 做 Grouping，產生一個 Map[machineID, List[RawData]]
     val groupedData = rawDataList.groupBy(_.machineID)
+
+    // 將上述的資料中每一台機台的資料做加總，產生一個 List[ResultTableRow]
     val aggreatedData = groupedData.map { case (machineID, data) =>
       val totalCountQty = data.map(_.countQty).sum
       val totalBadQty = data.map(_.badQty).sum
@@ -50,9 +92,16 @@ class LossRateQueryResult {
       ResultTableRow(machineID, totalCountQty, totalBadQty, lossRate, totalLossMoney)
     }
 
+    // 最後以損耗由小到大排序
     aggreatedData.toList.sortWith(_.lossRate > _.lossRate)
   }
 
+  /**
+   *  顯示查詢結果表格的 Binding
+   *
+   *  @param    tableData       損耗率結果的排名
+   *  @return                   用來 Binding
+   */
   def showDataTable(tableData: List[ResultTableRow]) = {
     val startDate = S.param("startDate").getOrElse("")
     val endDate = S.param("endDate").getOrElse("")
@@ -71,6 +120,9 @@ class LossRateQueryResult {
   }
 
 
+  /**
+   *  顯示查詢結果
+   */
   def render = {
     val startDateHolder = S.param("startDate")
     val endDateHolder = S.param("endDate")
