cd7fe3863fa7ce71edf8eaf5f72e250bb44f3ca7
Brian Hsu <brianhsu.hsu@gmail.com>
2016-06-14 14:05:31 +0800

    Fix page title.

--- src/main/scala/code/snippet/SiteName.scala
+++ src/main/scala/code/snippet/SiteName.scala
@@ -9,8 +9,8 @@ import net.liftweb.util.Helpers._
 class SiteName {
 
   def siteName = S.hostName match {
-    case "221.4.141.146" | "192.168.20.200" | "xgback.zhenhai.com.tw" => "謝崗廠"
-    case "218.4.250.102" | "192.168.3.2" | "szback.zhenhai.com.tw" => "蘇州廠"
+    case "221.4.141.146" | "192.168.20.200" | "xgback.zhenhai.com.tw" => "東莞台容"
+    case "218.4.250.102" | "192.168.3.2" | "szback.zhenhai.com.tw" => "蘇州台容"
     case _ => "台灣電容器"
   }
 
